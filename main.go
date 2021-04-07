0902c3d56aff0efaba97e5da1030447c0bd7496b
英士 <comu2e@gmail.com>
2021-04-07 21:57:18 +0900

    net/httpでサーバー立てて/hello /headers叩くとメッセージ返すようにする

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
-import("fmt"
+import (
+	"fmt"
 	"net/http"
 )
-func main() {
-	fmt.Println("Hello")
-	localhost := "http://localhost.com"
-	_,err := http.Get(localhost)
 
-	if err != nil{
-		fmt.Println("Error")
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "CCCC\n")
+}
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name,headers := range req.Header{
+		for _,h := range headers{
+			fmt.Fprintf(w,"%v : %v\n",name,h)
+		}
 	}
+}
 
+func main() {
+	http.HandleFunc("/hello",hello)
+	http.HandleFunc("/headers",headers)
+	http.ListenAndServe(":8090",nil)
 }
