1e2bdff245c25c1f97c8bf9085517bc41fde73bc
英士 <comu2e@gmail.com>
2021-04-10 00:13:26 +0900

    testの流れ整理中

---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestCreateUser(t *testing.T)  {
+//	input: id,[Username,FirstName,LastName,Email,Password,Phone,UserStatus]
+//  dbが開けるか
+//	usersテーブルのidを取得できるか。userがない場合、ある場合。
+//	userが存在する場合は、idが大きい数でも対応できるか
+//	dbにinputデータを登録できているか。存在しているかチェック
+//	idは重複していないか
+
+}
