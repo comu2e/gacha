0a0edb1bda82d08a15e2a84a4924d90fad5c0907
Eiji Takahashi <comu2e@gmail.com>
2021-04-23 00:01:39 +0900

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -16,19 +16,17 @@ CREATE TABLE  users
     Email      VARCHAR(100) NOT NULL,
     Password   VARCHAR(100) NOT NULL,
     Phone      VARCHAR(11)  NULL,
-    UserStatus TINYINT(1)
-                            NOT NULL,
-    xToken     VARCHAR(20)
-                            NOT NULL
+    UserStatus TINYINT(1) NOT NULL,
+    xToken     VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE user_character (
-      id INT  PRIMARY KEY NOT NULL,
-      user_id INT NOT NULL,
-      FOREIGN KEY (id)
-      REFERENCES users(id) ,
-      character_id
-      INT NOT NULL,
-      FOREIGN KEY (id)
-      REFERENCES characters(id)
+    id INT  PRIMARY KEY NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (id)
+    REFERENCES users(id) ,
+    character_id
+    INT NOT NULL,
+    FOREIGN KEY (id)
+    REFERENCES characters(id)
 );
